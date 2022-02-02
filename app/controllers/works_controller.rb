d79577c435efcbbbe6e127e89c9ad794163721de
santakukoumei <rx0gtrsv@gmail.com>
2022-02-02 22:45:31 +0900

    画像投稿機能

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,31 @@
 class WorksController < ApplicationController
+  before_action :set_work, except: [:index, :new, :create]
+  
+
   def index
+    @works = Work.includes(:user)
+  end
+
+  def new
+   @work = Work.new
+  end
+
+  def create
+     @work = Work.new(work_params)
+     if @work.save
+       redirect_to root_path
+     else
+       render :new
+     end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:title, :category_id, :concept, :explanation, :image).merge(user_id: current_user.id)
+  end
+
+  def set_work
+    @work = Work.find(params[:id])
   end
 end
